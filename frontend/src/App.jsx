6452e88a6caa83b5b14d87691350a6b2612eeed4
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-04-11 20:51:27 +0530

    context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from './Components/Login'
 import Signup from './Components/Signup'
+import { LoginContext } from './Context/UserContext'
+import Home from './Components/Home'
 function App() {
+  const [ name, setname] = useState('')
+  const [ ID , SetID] = useState('')
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/signup' element={<Signup/>} />
+    <> 
+      <LoginContext.Provider value={{name,setname, ID, SetID}}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/home' element={<Home/>}/>
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </LoginContext.Provider>
+
 
     </>
   )
