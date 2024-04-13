68752873d23ab906b9063adfe1e4121274be59ee
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-04-13 23:01:47 +0530

    Signup page

---
+++ frontend/src/Components/Signup.jsx
@@ -0,0 +1,55 @@
+import React, {  useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios"
+import { LoginContext } from '../Context/UserContext';
+function Signup() {
+  const { name, setname } = useContext(LoginContext)
+  const navigate = useNavigate()
+  const api = "http://localhost:3001/api/create"
+
+
+  const HandleSubmit = (e) => {
+    e.preventDefault()
+    axios.post(api, { name }, { withCredentials: true })
+      .then((res) => {
+        console.log(res)
+        navigate('/Home')
+
+      })
+      .catch((e) => console.log(e))
+  }
+
+
+  return (
+    <>
+      <div className='card shadow-xl bg-base-100 w-96 max-w-xl'>
+        <div className='card-body'>
+          <h2 className='card-title'>Signup</h2>
+          <form onSubmit={HandleSubmit}>
+            <div>
+              <input
+                type="text"
+                value={name}
+                onChange={e => setname(e.target.value)}
+                placeholder='Enter Name'
+                className='input input-bordered w-full max-w-xs'
+              />
+
+            </div>
+            <div className='card-actions justify-center py-3'>
+              <button type='submit' className='btn btn-success' >Signup</button>
+            </div>
+            <div className='card-actions justify-center'>
+              <h2>Already have an account? <Link className='text-warning' to={'/'}> Login</Link></h2>
+            </div>
+          </form>
+
+        </div>
+
+
+      </div>
+    </>
+  )
+}
+
+export default Signup
