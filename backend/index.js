2088aefeb5786bc15ec5bc5c558c8eb4c114a160
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-04-16 20:25:01 +0530

    Created separate folder for routes

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+
+const db = require('./models/conn')
+const cors = require('cors');
+db()
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true,
+    }
+
+))
+
+const port = process.env.PORT
+app.use(express.json())
+
+app.use('/',require('./routes/route'))
+
+
+
+
+app.listen(port, () => console.log(`server connected ${port}`))
