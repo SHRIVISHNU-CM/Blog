02f63ea948cc9595164442d0048c07ef120cccd7
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-04-17 22:46:02 +0530

    add edit myblogs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,20 +7,23 @@ import Home from './Components/Home'
 import Add from './Components/Add'
 import Profile from './Components/Profile'
 import Edit from './Components/Edit'
+import Myblogs from './Components/Myblogs'
 function App() {
-  const [ name, setname] = useState('')
-  const [ ID , SetID] = useState('')
+  const [name, setname] = useState('')
+  const [ID, SetID] = useState('')
+  const [len, setlen] = useState('')
   return (
-    <> 
-      <LoginContext.Provider value={{name,setname, ID, SetID}}>
+    <>
+      <LoginContext.Provider value={{ name, setname, ID, SetID, len, setlen }}>
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
-            <Route path='/home' element={<Home/>}/>
-            <Route path='/add' element={<Add/>}/>
-            <Route path='/edit/:id' element={<Edit/>}/>
-            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/home' element={<Home />} />
+            <Route path='/myblogs' element={<Myblogs />} />
+            <Route path='/add' element={<Add />} />
+            <Route path='/edit/:id' element={<Edit />} />
+            <Route path='/profile' element={<Profile />} />
           </Routes>
         </BrowserRouter>
       </LoginContext.Provider>
