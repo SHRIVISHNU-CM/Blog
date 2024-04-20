a7b2fd0bf7146babd293790d756670f088a17752
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-04-21 00:14:56 +0530

    error handler

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -2,62 +2,64 @@ import { useContext, useEffect, useState } from "react";
 import { LoginContext } from "../Context/UserContext";
 import axios from "axios";
 import NavBar from "./NavBar"
-import ReactHtmlParser  from "react-html-parser"
+import ReactHtmlParser from "react-html-parser"
 
 function Home() {
-    const { name, ID } = useContext(LoginContext)
     const [data, setdata] = useState([])
-    const [dataOne, setdataOne] = useState([])
-    
+    const [error, setError] = useState(false)
+
     useEffect(() => {
         axios.get('http://localhost:3001/api/create')
+
             .then((res) => {
                 console.log(res)
                 setdata(res.data)
             })
-        axios.get(`http://localhost:3001/api/find/${ID}`)
-            .then((res) => {
-                setdataOne(res.data.notes)
-                console.log(res.data)
-
+            .catch((e) => {
+                setError(true)
             })
 
     }, [])
-   
-    
+
+
 
     return (
         <>
-            <NavBar/>
-            <h1 className="card-title">{ID}</h1>
-            
+            <NavBar />
             {
-                data.map((el, i) => {
-                    return (
-                        <div key={i} className="card shadow-xl w-full bg-base-100  border my-10">
-                            <div className="card-body">
-                                <h1 className="card-title uppercase">{el.name}</h1>
-                                {el.notes.map(Data => {
-                                    return (
-                                        <div key={Data.id} className="card w-full">
-                                            <p><span className="font-semibold :">Title</span>: {Data.sub}</p>
-                                            <h1><span className="font-semibold" >Notes</span>: {ReactHtmlParser(Data.head)}</h1>
-
-                                            {/* <Link className="btn btn-success" to={`/edit/${Data._id}`}>Edit</Link> */}
-                                        </div>
-                                    )
-
-                                })}
-
-                            </div>
+                error ?
+                    <div className='card justify-center'>
+                        <div className='card-body'>
+                            <h1 className='card-title'>Error</h1>
+                            <Link to={'/'} className='btn text-white bg-gray-500 w-36'>Go To Login</Link>
 
                         </div>
 
+                    </div> :
+                    data.map((el, i) => {
+                        return (
+                            <div key={el._id} className="card shadow-xl mx-3 bg-base-100  my-10">
+                                <div className="card-body">
+                                    <h1 className="card-title uppercase">{el.name}</h1>
+                                    {el.notes.map(Data => {
+                                        return (
+                                            <div key={Data.id} className="px-4 py-2 bg-base-200 w-full rounded-md">
+                                                <p><span className="font-semibold :">Title</span>: {Data.sub}</p>
+                                                <p><span className="font-semibold" >Notes</span>: {ReactHtmlParser(Data.head)}</p>
+                                            </div>
+                                        )
 
-                    )
-                })
+                                    })}
+
+                                </div>
+
+                            </div>
+
+
+                        )
+                    })
             }
-           
+
         </>
     )
 }
