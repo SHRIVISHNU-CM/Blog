0530e67c258944a801fa0e51f97ff1dc468f56b0
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-04-21 00:30:57 +0530

    rearranged

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-
 const db = require('./models/conn')
 const cors = require('cors');
 db()
+app.use(express.json())
 app.use(cors(
     {
         origin: "http://localhost:5173",
@@ -14,9 +14,8 @@ app.use(cors(
 ))
 
 const port = process.env.PORT
-app.use(express.json())
 
-app.use('/',require('./routes/route'))
+app.use('/', require('./routes/route'))
 
 
 
