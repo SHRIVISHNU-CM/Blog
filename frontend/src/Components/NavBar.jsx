21d2fe74cf3fa5051856c01d1b3d83222f69aafb
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-04-21 00:13:58 +0530

    dark mode and text colour

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
-function NavBar(){
+function NavBar() {
+    const [theme, SetTheme] = useState(localStorage.getItem('theme'));
+
+    const HandleToggle = (e) => {
+        if (e.target.checked) {
+            SetTheme('dracula')
+        } else {
+            SetTheme('winter')
+        }
+    }
+    useEffect(() => {
+        localStorage.setItem('theme', theme)
+        const localTheme = localStorage.getItem('theme')
+        document.querySelector('html').setAttribute('data-theme', localTheme)
+    }, [theme])
     return (
         <>
-            <nav className="navbar bg-base-200">
+            <nav className="navbar bg-base-200 w-full">
                 <div className="navbar-start md:block lg:hidden">
                     <div className="dropdown">
                         <div tabIndex={0} className="btn btn-ghost btn-circle " role="button">
@@ -16,15 +31,29 @@ function NavBar(){
                             <li>
                                 <Link to={'/profile'}>Profile</Link>
                             </li>
+                            <li>
+                                <Link to={'/myblogs'}>My Blogs</Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
                 <div className="navbar-center lg:navbar-start">
-                    <h1 className="text-2xl">Blog</h1>
+                    <Link className="text-2xl text-orange-500 hover:cursor-pointer hover:text-orange-300" to={'/home'}>Blogs</Link>
                 </div>
-                <div className= "hidden md:hidden lg:navbar-end lg:block lg:gap-2  ">
-                    <Link className="btn btn-primary " to={'/add'}>Create</Link>
-                    <Link className="btn btn-accent" to={'/profile'}>Profile</Link>
+
+
+                <div className="hidden lg:flex lg:navbar-end gap-x-3">
+                    <Link to={'/myblogs'} className="text-orange-500 hover:text-orange-300" >My Blogs</Link>
+                    <Link className="text-orange-500 hover:text-orange-300" to={'/add'}>Create Blog</Link>
+                    <Link to={'/profile'} className="text-orange-500 hover:text-orange-300">Profile</Link>
+
+                </div>
+                <div className='navbar-end'>
+                    <label className="flex cursor-pointer gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+                        <input type="checkbox" value="synthwave" className="toggle theme-controller" onChange={HandleToggle} checked={theme === "winter" ? false : true} />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                    </label>
                 </div>
             </nav>
         </>
