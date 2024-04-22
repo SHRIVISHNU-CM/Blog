d66f785497ee3eb8c01a388a8018fa8ca78b4b34
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-04-23 00:33:43 +0530

    loading functionality

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,5 +1,4 @@
-import { useContext, useEffect, useState } from "react";
-import { LoginContext } from "../Context/UserContext";
+import {  useEffect, useState } from "react";
 import axios from "axios";
 import NavBar from "./NavBar"
 import ReactHtmlParser from "react-html-parser"
@@ -7,57 +6,68 @@ import ReactHtmlParser from "react-html-parser"
 function Home() {
     const [data, setdata] = useState([])
     const [error, setError] = useState(false)
+    const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get('http://localhost:3001/api/create')
+        const timer = setTimeout(() => {
+            setLoading(false)
+            axios.get('http://localhost:3001/api/create')
 
-            .then((res) => {
-                console.log(res)
-                setdata(res.data)
-            })
-            .catch((e) => {
-                setError(true)
-            })
+                .then((res) => {
+                    console.log(res)
+                    setdata(res.data)
+                })
+                .catch((e) => {
+                    setError(true)
+                })
+        }, 2000)
+        return () => { clearTimeout(timer) }
 
-    }, [])
+
+    }, [isLoading])
 
 
 
     return (
         <>
             <NavBar />
-            {
-                error ?
-                    <div className='card justify-center'>
-                        <div className='card-body'>
-                            <h1 className='card-title'>Error</h1>
-                            <Link to={'/'} className='btn text-white bg-gray-500 w-36'>Go To Login</Link>
+            {isLoading ?
+                <div className="flex justify-center items-center">
+                    <span className="loading loading-spinner text-secondary loading-lg"></span>
 
-                        </div>
+                </div>
+                : null}
+            {error ?
+                <div className='card justify-center'>
+                    <div className='card-body'>
+                        <h1 className='card-title'>Error</h1>
+                        <Link to={'/'} className='btn text-white bg-gray-500 w-36'>Go To Login</Link>
 
-                    </div> :
-                    data.map((el, i) => {
-                        return (
-                            <div key={el._id} className="card shadow-xl mx-3 bg-base-100  my-10">
-                                <div className="card-body">
-                                    <h1 className="card-title uppercase">{el.name}</h1>
-                                    {el.notes.map(Data => {
-                                        return (
-                                            <div key={Data.id} className="px-4 py-2 bg-base-200 w-full rounded-md">
-                                                <p><span className="font-semibold :">Title</span>: {Data.sub}</p>
-                                                <p><span className="font-semibold" >Notes</span>: {ReactHtmlParser(Data.head)}</p>
-                                            </div>
-                                        )
+                    </div>
 
-                                    })}
+                </div> :
+                data.map((el, i) => {
+                    return (
+                        <div key={el._id} className="card shadow-xl mx-3 bg-base-100  my-10">
+                            <div className="card-body">
+                                <h1 className="card-title uppercase">{el.name}</h1>
+                                {el.notes.map(Data => {
+                                    return (
+                                        <div key={Data.id} className="px-4 py-2 bg-base-200 w-full rounded-md">
+                                            <p><span className="font-semibold :">Title</span>: {Data.sub}</p>
+                                            <p><span className="font-semibold" >Notes</span>: {ReactHtmlParser(Data.head)}</p>
+                                        </div>
+                                    )
 
-                                </div>
+                                })}
 
                             </div>
 
+                        </div>
 
-                        )
-                    })
+
+                    )
+                })
             }
 
         </>
